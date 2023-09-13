a43e4f0c7a4bb5cb5ad56dc9f7264aa15a4382ce
VictorOPB <victor.bustos1999@gmail.com>
2023-09-13 17:21:24 -0300

    fix: load_data file path

--- modules/load_data.py
+++ modules/load_data.py
@@ -4,13 +4,13 @@ import pandas as pd
 def load_data():
 
     #load fed rate
-    rate = pd.read_parquet("dataset/fed_rate.parquet")
+    rate = pd.read_parquet("dataset/US/fed_rate.parquet")
 
     #load sp500_components
-    sp = pd.read_parquet("dataset/sp_comp.parquet")
+    sp = pd.read_parquet("dataset/US/sp_comp.parquet")
 
     #load prices_sp
-    melt_prices = pd.read_parquet("dataset/prices_sp.parquet")
+    melt_prices = pd.read_parquet("dataset/US/prices_sp.parquet")
     df_prices = melt_prices.pivot_table(index = "Date", columns = "Ticker", values = "value")
 
     #create a dictionary with data
